d905b79226a3e1670811634756e0a33ee088e9d8
Samwuel simiyu <samwuels254@gmail.com>
2024-04-08 03:53:20 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,31 +3,25 @@
 subreddit module
 """
 
+from requests import get
+
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers """
-    from requests import get
+    """
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = {'user-agent': 'my-app/0.0.1'}
-
-    r = get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        js = r.json()
+        return results.get('data').get('subscribers')
 
-    except ValueError:
+    except Exception:
         return 0
-
-    data = js.get("data")
-
-    if data:
-        sub_count = data.get("subscribers")
-        if sub_count:
-            return sub_count
-
-    return 0
