d742bd41edfa3884efc4438d9c209a50c51acb7f
Samwuel simiyu <samwuels254@gmail.com>
2024-05-11 05:18:20 -0700

    Top ten, N/B it only works on my machine

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Top ten hot spots
+"""
+
+
+def top_ten(subreddit):
+    """prints the top ten list"""
+
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+
+    headers = {'user-agent': 'kamikaze/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        print(None)
+        return None
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        print(None)
+        return None
+
+    try:
+
+        data = js.get("data")
+        children = data.get("children")
+        for child in children[:10]:
+            post = child.get("data")
+            print(post.get("title"))
+
+    except Exception as e:
+        print(None)
